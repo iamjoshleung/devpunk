9611b61d54241a77e495173059c530d0dd4f2260
joshuajazleung <joshuajazleung@gmail.com>
2017-04-29 18:29:54 -0700

    changed placehold.it url to serve images on https

--- src/mixins/ProjectData.vue
+++ src/mixins/ProjectData.vue
@@ -10,19 +10,19 @@ export default {
           body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, 1',
           thumbnails: {
             small: [
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330'
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330'
             ],
             medium: [
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900'
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900'
             ], 
             high: [
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400'
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400'
             ]
           },
           stacks: ['Ruby on Rails', 'VueJS', 'Redux', 'Heroku']
@@ -34,19 +34,19 @@ export default {
           body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, 2',
           thumbnails: {
             small: [
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330'
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330'
             ],
             medium: [
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900'
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900'
             ], 
             high: [
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400'
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400'
             ]
           },
           stacks: ['Ruby on Rails', 'VueJS', 'Redux', 'Heroku'],
@@ -59,19 +59,19 @@ export default {
           body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, 3',
           thumbnails: {
             small: [
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330'
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330'
             ],
             medium: [
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900'
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900'
             ], 
             high: [
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400'
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400'
             ]
           },
           stacks: ['Ruby on Rails', 'VueJS', 'Redux', 'Heroku']
@@ -83,19 +83,19 @@ export default {
           body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, 3',
           thumbnails: {
             small: [
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330',
-              'http://placehold.it/330x330'
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330',
+              'https://placehold.it/330x330'
             ],
             medium: [
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900',
-              'http://placehold.it/900x900'
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900',
+              'https://placehold.it/900x900'
             ], 
             high: [
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400',
-              'http://placehold.it/1400x1400'
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400',
+              'https://placehold.it/1400x1400'
             ]
           },
           stacks: ['Ruby on Rails', 'VueJS', 'Redux', 'Heroku']
